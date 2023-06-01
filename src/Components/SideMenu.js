46e79d3359af705783e1172c47c1c36f6d94140a
Jimmy <jimmycshaw@hotmail.co.nz>
2023-06-01 23:02:42 +1200

    Application logo added.

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../App.css';
+import logo from "../basic-react-app-logo.png";
 
 import { SideMenuItems } from './SideMenuItems';
 
 function SideMenu() {
   return (
     <div className = "side-menu">
+      <div>
+        <img src = { logo } width={ 150 } className = "app-logo"></img>
+      </div>
       <ul className = "side-menu-list">
         { SideMenuItems.map((val, key) => {
           return <li key = { key } onClick = {() => { window.location.pathname = val.link}} 
