dd16cf12bdf072f4ae361d6ef99f9497cc6b0857
Jimmy <jimmycshaw@hotmail.co.nz>
2023-06-04 17:01:56 +1200

    Calculator functionality and UI started.

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import '../App.css'
 
 function Calculator() {
   return (
-    <div className = "window-body">Calculator</div>
+    <div className = "window-body">
+      <div className = "center-screen">
+        <div className = "calc-body">
+          <div>
+            <input className = "calc-input"></input>
+          </div>
+          <div>
+            <button className = "calc-button">1</button>
+            <button className = "calc-button">2</button>
+            <button className = "calc-button">3</button>
+            <button className = "calc-button">0</button>
+          </div>
+          <div className = "calc-spacer"></div>
+          <div>
+            <button className = "calc-button">4</button>
+            <button className = "calc-button">5</button>
+            <button className = "calc-button">6</button>
+            <button className = "calc-button">+</button>
+          </div>
+          <div className = "calc-spacer"></div>
+          <div>
+            <button className = "calc-button">7</button>
+            <button className = "calc-button">8</button>
+            <button className = "calc-button">9</button>
+            <button className = "calc-button">-</button>
+          </div>
+          <div className = "calc-spacer"></div>
+          <div>
+            <button className = "calc-button">x</button>
+            <button className = "calc-button">/</button>
+            <button className = "calc-button">LB</button>
+            <button className = "calc-button">RB</button>
+          </div>
+          <div className = "calc-spacer"></div>
+          <div>
+            <button className = "calc-button">=</button>
+            <button className = "calc-button">B</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Calculator
+export default Calculator;
--- src/Pages/Counter.js
+++ src/Pages/Counter.js
@@ -4,14 +4,24 @@ import '../App.css';
 import  { useState } from 'react';
 
 function Counter() {
+  const [val, setVal] = useState(0);
+
+  const increment = () => {
+    setVal(val => val + 1);
+  };
+
+  const decrement = () => {
+    setVal(val => val - 1);
+  };
+
   return (
     <div className = "window-body">
       <div className = "center-screen">
-          <button className = "inc-dec">-</button>
+          <button className = "inc-dec" onClick = { decrement }>-</button>
           <div className = "spacer"></div>
-          <h2>Counter</h2>
+          <h2>{ val }</h2>
           <div className = "spacer"></div>
-          <button className = "inc-dec">+</button>
+          <button className = "inc-dec" onClick = { increment }>+</button>
       </div>
     </div>
   );
